4f149e854e6eb4dde194d9e7e941a236de75233b
Nuzhat Azra <nuzhat08@github.com>
2018-02-28 18:46:11 -0700

    meh

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -16,3 +16,16 @@
 hello <- function() {
   print("Hello, world!")
 }
+
+
+densityPlot <- function(data,variables, outline, fill){
+  attach(data)
+  ggplot(data)+
+    geom_density(aes(variables), outline, fill)
+}
+
+
+densityPlot(wine, points, "blue", "cyan")
+
+ggplot(wine)+
+  geom_density(aes(points), colour= "blue", fill= "cyan")
